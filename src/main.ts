5a1ded400cf9e3df7e2169ff636a21d43bf0bfe8
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-11-13 21:02:34 +0100

    Rectangle drew

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 import './style.css'
+
+const canvas = document.getElementById('app') as HTMLCanvasElement;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext('2d')!;
+
+window.ctx = ctx;
+window.canvas = canvas;
+
+const drawRectangle = (ctx: CanvasRenderingContext2D, color: string, origin: [number, number], dimensions: [number, number]): void => {
+    ctx.fillStyle = color;
+    ctx.fillRect(...origin, ...dimensions);
+}
+
+drawRectangle(ctx, "green", [20, 40], [300, 100]);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+interface Window {
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+}
