61909c6995995a258a1bd98501a2b20316906e28
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-11-14 09:44:57 +0100

    Added drawStrokedTriangle action, fixed coordinates issue

--- src/menu/menu.ts
+++ src/menu/menu.ts
@@ -1,15 +1,30 @@
 import type { Coordinates } from '../types/core.type.ts';
-import { drawFilledCircle, drawFilledTriangle } from '../shapes/paths.ts';
+import { drawFilledCircle, drawFilledTriangle, drawStrokedTriangle } from '../shapes/paths.ts';
 
 const menu = document.getElementById('menu') as HTMLElement;
 
 const handleTriangleFillAction = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, signal: AbortSignal) => {
   const trianglePoints = [] as Coordinates[];
   canvas.addEventListener('click', (event: MouseEvent) => {
-    const { clientX, clientY } = event;
-    trianglePoints.push([clientX, clientY] as Coordinates);
+    const { offsetX, offsetY } = event;
+    trianglePoints.push([offsetX, offsetY] as Coordinates);
     if (trianglePoints.length === 3) {
       drawFilledTriangle(ctx, {
+        color: 'blue',
+        points: trianglePoints as [Coordinates, Coordinates, Coordinates]
+      });
+      trianglePoints.length = 0;
+    }
+  }, { signal });
+}
+
+const handleTriangleStrokeAction = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, signal: AbortSignal) => {
+  const trianglePoints = [] as Coordinates[];
+  canvas.addEventListener('click', (event: MouseEvent) => {
+    const { offsetX, offsetY } = event;
+    trianglePoints.push([offsetX, offsetY] as Coordinates);
+    if (trianglePoints.length === 3) {
+      drawStrokedTriangle(ctx, {
         color: 'blue',
         points: trianglePoints as [Coordinates, Coordinates, Coordinates]
       });
@@ -21,17 +36,18 @@ const handleTriangleFillAction = (canvas: HTMLCanvasElement, ctx: CanvasRenderin
 const handleCircleFillAction = (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, signal: AbortSignal) => {
     const circlePoints = [] as Coordinates[];
     canvas.addEventListener('click', (event: MouseEvent) => {
-      const { clientX, clientY } = event;
-      circlePoints.push([clientX, clientY] as Coordinates);
+      const { offsetX, offsetY } = event;
+      circlePoints.push([offsetX, offsetY] as Coordinates);
       if (circlePoints.length === 2) {
         drawFilledCircle(ctx, { color: 'pink', points: circlePoints as [Coordinates, Coordinates] });
         circlePoints.length = 0;
       }
-    })
+    }, { signal });
 }
 
 const actionMap: Record<string, (canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D, signal: AbortSignal) => void> = {
   triangleFill: handleTriangleFillAction,
+  triangleStroke: handleTriangleStrokeAction,
   circleFill: handleCircleFillAction,
 }
 
--- src/shapes/paths.ts
+++ src/shapes/paths.ts
@@ -1,20 +1,37 @@
-import type { Coordinates } from "../types/core.type.ts";
+import type { Coordinates } from '../types/core.type.ts';
 import { calculateDistance } from '../math/distance.ts';
 
+const drawTriangle = (ctx: CanvasRenderingContext2D, points: [Coordinates, Coordinates, Coordinates]) => {
+  ctx.beginPath();
+  ctx.moveTo(...points[0]);
+  ctx.lineTo(...points[1]);
+  ctx.lineTo(...points[2]);
+}
+
 export const drawFilledTriangle = (ctx: CanvasRenderingContext2D,
-                                   { color, points: [firstPoint, secondPoint, thirdPoint] }: {
-                                     color: string,
-                                     points: [Coordinates, Coordinates, Coordinates]
+                                   { color, points }: {
+                                     color: string, points: [Coordinates, Coordinates, Coordinates]
                                    }) => {
   ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(...firstPoint);
-  ctx.lineTo(...secondPoint);
-  ctx.lineTo(...thirdPoint);
+  drawTriangle(ctx, points);
   ctx.fill();
 }
 
-export const drawFilledCircle = (ctx: CanvasRenderingContext2D, { color, points: [centerPoint, outerPoint]}: { color: string, points: [Coordinates, Coordinates] } = {}) => {
+export const drawStrokedTriangle = (ctx: CanvasRenderingContext2D,
+                                    { color, points }: {
+                                      color: string,
+                                      points: [Coordinates, Coordinates, Coordinates]
+                                    }) => {
+  ctx.strokeStyle = color;
+  drawTriangle(ctx, points);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+export const drawFilledCircle = (ctx: CanvasRenderingContext2D, { color, points: [centerPoint, outerPoint] }: {
+  color: string,
+  points: [Coordinates, Coordinates]
+}) => {
   ctx.fillStyle = color;
   const radius = calculateDistance(centerPoint, outerPoint);
   ctx.beginPath();
