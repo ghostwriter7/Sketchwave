d15b2d5a33d61941b098ca132c579dff912f4018
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-11-15 14:39:46 +0100

    Layer mechanism recreated

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,8 +1,32 @@
+import { useGlobalContext } from './global-provider.tsx';
+import type { Layer } from './types/core.type.ts';
+
 const Canvas = () => {
-  const canvasEl = <canvas></canvas> as HTMLCanvasElement;
-  canvasEl.width = innerWidth;
-  canvasEl.height = innerHeight;
-  return canvasEl
+  const { state } = useGlobalContext();
+
+  const handleClick = (event: MouseEvent) => {
+    const { offsetX, offsetY } = event;
+    console.log('Click at ', offsetX, offsetY);
+  }
+
+  const handleMouseMove = (event: MouseEvent) => {
+    const { offsetX, offsetY } = event;
+    console.log('Mouse move at ', offsetX, offsetY);
+  }
+
+  const canvasEl = <canvas onClick={handleClick} onMouseMove={handleMouseMove} width={innerWidth} height={innerHeight}></canvas> as HTMLCanvasElement;
+
+  const ctx = canvasEl.getContext('2d') as CanvasRenderingContext2D;
+
+  const render = () => {
+    ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    state.layers.forEach((layer: Layer) => layer.draw(ctx));
+    requestAnimationFrame(render);
+  }
+
+  render();
+
+  return canvasEl;
 }
 
 export default Canvas;
--- src/global-provider.tsx
+++ src/global-provider.tsx
@@ -1,10 +1,17 @@
 import { createContext, useContext } from 'solid-js';
 import { createStore } from 'solid-js/store';
+import type { Layer } from './types/core.type.ts';
 
 const GlobalContext = createContext();
 
 export const GlobalProvider = (props) => {
-  const [state, setState] = createStore<{ activeTool?: 'rect' | 'triangle' | 'circle' }>({ activeTool: undefined });
+  const [state, setState] = createStore<{
+    activeTool?: 'rect' | 'triangle' | 'circle';
+    layers: Layer[]
+  }>({
+    activeTool: undefined,
+    layers: []
+  });
   const setActiveTool = (activeTool: 'rect' | 'triangle' | 'circle') => setState('activeTool', activeTool);
 
   return (
