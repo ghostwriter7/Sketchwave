972b330b34c214cb1ab73c9f4d473751a948c4a5
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-06 08:07:43 +0100

    Rotate indicator drew

--- src/render/tools/resizer/Resizer.ts
+++ src/render/tools/resizer/Resizer.ts
@@ -1,5 +1,6 @@
 import { Point } from '../../../types/Point.ts';
 import { ThemeHelper } from '../../../helpers/theme.helper.ts';
+import { toRadians } from '../../../math/to-radians.ts';
 
 export class Resizer {
   private readonly canvas: HTMLCanvasElement;
@@ -9,11 +10,10 @@ export class Resizer {
   private boxWidth!: number;
   private boxHeight!: number;
 
-  private readonly indicatorWidth = 10;
-  private readonly indicatorHeight = 10;
+  private readonly indicatorDimension = 10;
 
-  private readonly halfIndicatorWidth = this.indicatorWidth / 2;
-  private readonly halfIndicatorHeight = this.indicatorHeight / 2;
+  private readonly halfIndicatorWidth = this.indicatorDimension / 2;
+  private readonly halfIndicatorHeight = this.indicatorDimension / 2;
 
   constructor(width: number, height: number) {
     this.canvas = document.createElement('canvas');
@@ -31,25 +31,51 @@ export class Resizer {
     this.boxWidth = width;
     this.boxHeight = height;
 
-    this.ctx.lineWidth = 3;
-    this.ctx.setLineDash([2, 3]);
+
     this.ctx.strokeStyle = ThemeHelper.getColor('clr-primary');
     this.currentPath = new Path2D();
     this.currentPath.rect(origin.x, origin.y, width, height);
     requestAnimationFrame(this.drawAndAnimateBox)
   }
 
-  private drawAndAnimateBox(): void {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  private drawDashedBox(): void {
+    this.ctx.lineWidth = 3;
+    this.ctx.setLineDash([2, 3]);
     this.ctx.lineDashOffset = this.ctx.lineDashOffset == 10 ? 0 : this.ctx.lineDashOffset + 1;
     this.ctx.stroke(this.currentPath!);
+  }
+
+  private drawAndAnimateBox(): void {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.drawDashedBox();
     this.drawResizeIndicators();
+    this.drawRotateHandle();
     requestAnimationFrame(this.drawAndAnimateBox);
   }
 
+  private drawRotateHandle(): void {
+    this.ctx.beginPath();
+    this.ctx.setLineDash([]);
+    this.ctx.lineWidth = 3;
+    this.ctx.lineCap = this.ctx.lineJoin =  'round'
+    const startX = this.origin.x + 15;
+
+    const startY = this.origin.y + this.boxHeight - 15;
+
+    this.ctx.moveTo(startX + 10, startY);
+    this.ctx.arc(startX, startY, 10, 0, toRadians(325));
+    this.ctx.lineTo(startX + 1, startY -2);
+
+    this.ctx.moveTo(startX + 10, startY);
+    this.ctx.arc(startX, startY, 10, 0, toRadians(325));
+    this.ctx.lineTo(startX + 6, startY - 15);
+
+    this.ctx.stroke();
+  }
+
   private drawResizeIndicators(): void {
-    const leftX = this.origin.x - this.indicatorWidth;
-    const topY = this.origin.y - this.indicatorHeight;
+    const leftX = this.origin.x - this.indicatorDimension;
+    const topY = this.origin.y - this.indicatorDimension;
     const centerX = this.origin.x + this.boxWidth * .5 - this.halfIndicatorWidth;
     const centerY = this.origin.y + this.boxHeight * .5 - this.halfIndicatorHeight;
     const endX = this.origin.x + this.boxWidth;
@@ -68,10 +94,14 @@ export class Resizer {
       new Point(endX, endY)
     ];
 
+    this.ctx.lineDashOffset = 0;
+    this.ctx.setLineDash([]);
+
     points.forEach(({ x, y }: Point) => {
       this.ctx.beginPath();
-      this.ctx.fillStyle = ThemeHelper.getColor('clr-primary');
-      this.ctx.fillRect(x, y, this.indicatorWidth, this.indicatorHeight);
+      this.ctx.fillStyle = '#fff';
+      this.ctx.fillRect(x, y, this.indicatorDimension, this.indicatorDimension);
+      this.ctx.strokeRect(x, y, this.indicatorDimension, this.indicatorDimension);
     });
   }
 }
