0435363b2568cb19afd5ee6de97307d0149790d0
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-08 12:25:24 +0100

    Improved WiggleLineBrush

--- src/render/tools/brushes/wiggle-line-brush/WiggleLineBrush.ts
+++ src/render/tools/brushes/wiggle-line-brush/WiggleLineBrush.ts
@@ -6,54 +6,49 @@ import { Point } from '../../../../types/Point.ts';
 import { Vec2 } from '../../../../types/Vec2.ts';
 
 export class WiggleLineBrush extends SimpleBrush {
+  private path = new Path2D();
 
   constructor(toolState: ToolState, layerFacade: LayerFacade) {
-    super(toolState, layerFacade);
+    super(toolState, layerFacade)
   }
 
   public tryCreateLayer(): void {
     if (this.points.length < 2) return;
 
-    const points = this.points;
-
-    this.createLayer((ctx: CanvasRenderingContext2D) => {
-      ctx.beginPath();
-      for (let i = 1; i < points.length; i++) {
-        const currentPoint = points.at(i)!;
-        const previousPoint = points[i - 1];
-        WiggleLineBrush.drawWiggleLineBetweenTwoPoints(ctx, previousPoint, currentPoint, i % 2 == 0);
-      }
-    });
+    const path = this.path;
+    this.createLayer((ctx: CanvasRenderingContext2D) => ctx.stroke(path));
   }
 
   protected resetState(): void {
     super.resetState();
-    this.ctx.beginPath();
+    this.path = new Path2D();
   }
 
   protected override renderPreview(): void {
+    if (this.points.length == 1) {
+      this.path.moveTo(this.points[0].x, this.points[0].y);
+    }
+
     if (this.points.length < 2) return;
 
     const currentPoint = this.points.at(-1)!;
     const previousPoint = this.points[this.lastPointIndex];
 
-    WiggleLineBrush.drawWiggleLineBetweenTwoPoints(this.ctx, previousPoint, currentPoint, this.lastPointIndex % 2 == 0);
-    this.lastPointIndex++;
-  }
-
-  private static drawWiggleLineBetweenTwoPoints(ctx: CanvasRenderingContext2D, previousPoint: Point, currentPoint: Point, clockwise: boolean): void {
     const distance = calculateDistance(currentPoint, previousPoint);
     const radius = distance / 2;
     const midPoint = Point.midPoint(currentPoint, previousPoint);
 
     const midPointVec = new Vec2(midPoint.x - previousPoint.x, midPoint.y - previousPoint.y);
 
+    const clockwise = this.lastPointIndex % 2 == 0;
     const perpVec = (clockwise ? midPointVec.cwPerp() : midPointVec.ccwPerp()).scale(6);
 
     const controlX = midPoint.x + perpVec.x;
     const controlY = midPoint.y + perpVec.y;
 
-    ctx.arcTo(controlX, controlY, currentPoint.x, currentPoint.y, radius)
-    ctx.stroke();
+    this.path.arcTo(controlX, controlY, currentPoint.x, currentPoint.y, radius)
+    this.ctx.stroke(this.path);
+
+    this.lastPointIndex++;
   }
 }
