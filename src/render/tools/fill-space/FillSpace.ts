370d80ca197d01634f5f39d3de97089bc66e1d40
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-09 14:37:18 +0100

    Flood-fill algorithm advanced

--- src/render/tools/fill-space/FillSpace.ts
+++ src/render/tools/fill-space/FillSpace.ts
@@ -27,17 +27,15 @@ export class FillSpace extends ToolHandler {
   }
 
   private processPointsIteratively(targetColor: ImageData, startPoint: Point): void {
-    const queue = [[startPoint.x, startPoint.y]] as [number, number][];
+    const width = this.width;
+    const height = this.height;
+    const firstPointIndex = startPoint.y * width + startPoint.x;
+
+    const queue = [firstPointIndex] as number[];
     const imageData = this.ctx.getImageData(0, 0, this.width, this.height);
     const array = new Uint8ClampedArray(imageData.data.buffer);
     const [red, green, blue] = this.toolState.color;
-
-    const width = this.width;
-    const height = this.height;
-
     const visitedPoints = new Uint8Array(width * height);
-
-    const isVisited = (index: number) => visitedPoints[index] == 1
 
     const targetColorAsDigit =
       (targetColor.data[0] << 24) |
@@ -45,38 +43,39 @@ export class FillSpace extends ToolHandler {
       (targetColor.data[2] << 8) |
       targetColor.data[3];
 
+    const offsets = this.vectorsForSiblingPoints.map(([dx, dy]) => dy * width + dx);
+
     console.time('LOOP')
     while (queue.length > 0) {
-      const [x, y] = queue.shift()!;
+      const rawIndex = queue.shift()!;
 
-      const rawIndex = y * width + x;
+      if (visitedPoints[rawIndex] == 1) continue;
+
       const index = rawIndex * 4;
-
-      if (isVisited(rawIndex)) continue;
 
       visitedPoints[rawIndex] = 1
 
+      const secondIndex = index + 1;
+      const thirdIndex = index + 2;
+      const fourthIndex = index + 3;
+
       const currentPixelColor =
         (imageData.data[index] << 24) |
-        (imageData.data[index + 1] << 16) |
-        (imageData.data[index + 2] << 8) |
-        imageData.data[index + 3];
+        (imageData.data[secondIndex] << 16) |
+        (imageData.data[thirdIndex] << 8) |
+        imageData.data[fourthIndex];
 
       if (currentPixelColor == targetColorAsDigit) {
         array[index] = red;
-        array[index + 1] = green;
-        array[index + 2] = blue;
-        array[index + 3] = 255;
+        array[secondIndex] = green;
+        array[thirdIndex] = blue;
+        array[fourthIndex] = 255;
 
-        for (const [dx, dy] of this.vectorsForSiblingPoints) {
-          const point = [x + dx, y + dy] as [number, number];
+        for (const offset of offsets) {
+          const neighbourIndex = rawIndex + offset;
 
-          if (
-            point[0] >= 0 &&
-            point[0] <= width &&
-            point[1] >= 0 &&
-            point[1] <= height) {
-            queue.push(point);
+          if (neighbourIndex >= 0 && neighbourIndex < visitedPoints.length && !visitedPoints[neighbourIndex]) {
+            queue.push(neighbourIndex);
           }
         }
       }
