d892aadad68b8ecf615c836faa96330059025f19
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-18 13:56:23 +0100

    Some experiments with WebGL added

--- src/playground/playground.tsx
+++ src/playground/playground.tsx
@@ -6,5 +6,65 @@ export const Playground = () => {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
+  const vsSource = `
+    attribute vec4 aVertexPosition;
+    uniform mat4 uModelViewMatrix;
+    uniform mat4 uProjectionMatrix;
+
+    void main() {
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+    }
+  `;
+
+  const fsSource = `
+    void main() {
+      gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    }
+  `;
+
+  function initShaderProgram(gl: WebGLRenderingContext, vsSource: string, fsSource: string): WebGLProgram | null {
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader!);
+    gl.attachShader(shaderProgram, fragmentShader!);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+      console.error(`Unable to initialize the shader program:  ${gl.getProgramInfoLog(shaderProgram)}`);
+      return null;
+    }
+
+    return shaderProgram;
+  }
+
+  function loadShader(gl: WebGLRenderingContext, type: number, source: string): WebGLShader | null {
+    const shader = gl.createShader(type)!;
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      console.error(`An error occurred compiling the shaders: ${gl.getShaderInfoLog(shader)}`);
+      gl.deleteShader(shader);
+      return null;
+    }
+
+    return shader;
+  }
+
+  const program = initShaderProgram(gl, vsSource, fsSource)!;
+
+  const programInfo = {
+    program,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(program, 'aVertexPosition'),
+    },
+    uniformLocations: {
+      modelViewMatrix: gl.getUniformLocation(program, 'uModelViewMatrix'),
+      projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
+    }
+  }
+
   return canvas;
 }
