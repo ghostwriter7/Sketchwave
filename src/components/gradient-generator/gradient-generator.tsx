b8ed75dfd242f3bf15d85a25e93b532bd0caebba
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-19 16:21:48 +0100

    Added ability to insert new stops and move them around

--- src/components/gradient-generator/gradient-generator.tsx
+++ src/components/gradient-generator/gradient-generator.tsx
@@ -2,10 +2,10 @@ import { GradientPreview } from './gradient-preview.tsx';
 import { Color } from '../../types/Color.ts';
 import { createStore } from 'solid-js/store';
 import styles from './gradient-generator.module.css';
-import { createContext, type ParentProps, useContext } from 'solid-js';
+import { createContext, createMemo, createUniqueId, type ParentProps, useContext } from 'solid-js';
 import { GradientInput } from './gradient-input.tsx';
 
-export type GradientDefinition = { color: Color, stop: number };
+export type GradientDefinition = { color: Color, id: string; stop: number };
 export type GradientDefinitions = GradientDefinition[];
 
 type GradientStore = {
@@ -15,23 +15,42 @@ type GradientStore = {
 
 export type GradientType = 'linear' | 'radial' | 'conic';
 
-export const GradientContext = createContext<{ state: GradientStore }>();
+export const GradientContext = createContext<{
+  positionStop: (id: string, stop: number) => void;
+  insertStop: (stop: number, color: Color) => string;
+  sortedGradientDefinitions: () => GradientDefinition[];
+  state: GradientStore
+}>();
 
 export const useGradientContext = () => useContext(GradientContext)!;
 
 export const GradientGenerator = () => {
-  const [state, _setState] = createStore<GradientStore>({
+  const [state, setState] = createStore<GradientStore>({
     gradientDefinitions: [
       { color: Color.fromHsl(174, 100, 50), stop: 0 },
       { color: Color.fromHsl(280, 100, 50), stop: .5 },
       { color: Color.fromHsl(212, 100, 50), stop: 1 },
-    ],
+    ].map((def) => ({...def, id: createUniqueId() })),
     gradientType: 'linear'
   });
 
+  const positionStop = (id: string, stop: number) => {
+    const stopIndex = state.gradientDefinitions.findIndex((def) => def.id === id);
+    const updatedStop = { ...state.gradientDefinitions[stopIndex] }!;
+    updatedStop.stop = stop || updatedStop.stop;
+    setState('gradientDefinitions', state.gradientDefinitions.with(stopIndex, updatedStop));
+  }
+
+  const insertStop = (stop: number, color: Color) => {
+    const id = createUniqueId();
+    setState('gradientDefinitions', [...state.gradientDefinitions, { stop, color, id}]);
+    return id;
+  }
+
+  const sortedGradientDefinitions = createMemo(() => [...state.gradientDefinitions].sort((a, b) => a.stop - b.stop));
 
   const Provider = (props: ParentProps) =>
-    <GradientContext.Provider value={{ state }}>{props.children}</GradientContext.Provider>;
+    <GradientContext.Provider value={{ state, insertStop, positionStop, sortedGradientDefinitions }}>{props.children}</GradientContext.Provider>;
 
   return <div class={styles['gradient-generator']}>
     <Provider>
--- src/components/gradient-generator/gradient-input.tsx
+++ src/components/gradient-generator/gradient-input.tsx
@@ -1,28 +1,58 @@
 import { GradientPreview } from './gradient-preview.tsx';
 import styles from './gradient-generator.module.css';
-import { For } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import { useGradientContext } from './gradient-generator.tsx';
+import { clampValue } from '../../math/clamp-value.ts';
+import { Color } from '../../types/Color.ts';
 
 export const GradientInput = () => {
-  const { state } = useGradientContext();
+  const { insertStop, positionStop, sortedGradientDefinitions } = useGradientContext();
+  const [activeIndicatorId, setActiveIndicatorId] = createSignal<string | null>(null);
+  const [draggedIndicatorId, setDraggedIndicatorId] = createSignal<string | null>(null);
   const width = 400;
   const indicatorHeight = `70px`;
   const indicatorWidth = 10;
+  const startMargin = 50;
 
-  return <div class={styles['gradient-input']}>
-    <GradientPreview height={50} width={width}/>
-    <For each={state.gradientDefinitions}>
-      {(({ color, stop }) =>
+  const stopDragging = () => setDraggedIndicatorId(null);
+
+  document.addEventListener('mouseup', stopDragging);
+
+  return <div
+    class={styles['gradient-input']}
+    onClick={(event: MouseEvent) => {
+      const id = insertStop((event.clientX - startMargin) / width, new Color(255, 255, 255));
+      setActiveIndicatorId(id);
+    }}
+    onMouseMove={(event: MouseEvent) => {
+      const id = draggedIndicatorId();
+      if (id) {
+        const offset = clampValue(event.clientX - startMargin, 0, 400) / width;
+        positionStop(id, offset);
+      }
+    }}
+    onMouseLeave={stopDragging}
+  >
+    <For each={sortedGradientDefinitions()}>
+      {(({ color, id, stop }) =>
         <span
           class={styles['stop-indicator']}
+          classList={{
+            [styles.active]: activeIndicatorId() === id
+          }}
           style={{
             background: color.toString(),
             height: indicatorHeight,
             width: `${indicatorWidth}px`,
-            left: `${stop * width - indicatorWidth / 2}px`,
-            top: `${-indicatorWidth}px`
+            transform: `translate(${stop * width - indicatorWidth / 2}px, ${-indicatorWidth}px)`,
+          }}
+          onMouseDown={() => setDraggedIndicatorId(id)}
+          onClick={(event: MouseEvent) => {
+            event.stopPropagation();
+            setActiveIndicatorId(id);
           }}
         ></span>)}
     </For>
+    <GradientPreview height={50} width={width}/>
   </div>
 }
