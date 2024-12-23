a3e7eb7f408eb2a40554e65084d62bbf6f4c0043
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-23 09:45:27 +0100

    Added ability to disable entire select

--- src/components/form-controls/select/select.tsx
+++ src/components/form-controls/select/select.tsx
@@ -2,6 +2,7 @@ import { type Accessor, createUniqueId, For, type VoidProps } from 'solid-js';
 import styles from './select.module.css';
 
 export type SelectProps<T = string | number> = {
+  disabled?: Accessor<boolean>;
   options: { disabled?: Accessor<boolean>; label: string; value: T }[];
   label: string;
   value: T;
@@ -14,13 +15,18 @@ export const Select = (props: VoidProps<SelectProps>) => {
   return <div class={styles.wrapper}>
     <label for={uniqueId}>{props.label}</label>
     <select
+      disabled={props.disabled?.()}
       class={styles.select}
       id={uniqueId}
       onChange={(event: Event) => props.onChange((event.target as HTMLSelectElement).value)
       }>
       <For each={props.options}>
         {({ value, label, disabled }) =>
-          <option disabled={disabled?.()} value={value}>{label}</option>}
+          <option
+            disabled={disabled?.()}
+            value={value}
+            selected={value === props.value}
+          >{label}</option>}
       </For>
     </select>
   </div>
