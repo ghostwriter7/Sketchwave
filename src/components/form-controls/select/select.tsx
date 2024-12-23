4d7a311c17c1cc1e79baa9296cacb01a79b0eb9c
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-23 09:06:29 +0100

    Select cmp created

---
+++ src/components/form-controls/select/select.tsx
@@ -0,0 +1,23 @@
+import { type Accessor, createUniqueId, For, type VoidProps } from 'solid-js';
+import styles from './select.module.css';
+
+export type SelectProps<T = string | number> = {
+  options: { disabled?: Accessor<boolean>; label: string; value: T }[];
+  label: string;
+  value: T;
+  onChange: (value: T) => void;
+}
+
+export const Select = (props: VoidProps<SelectProps>) => {
+  const uniqueId = createUniqueId();
+
+  return <div class={styles.wrapper}>
+    <label for={uniqueId}>{props.label}</label>
+    <select class={styles.select} id={uniqueId}>
+      <For each={props.options}>
+        {({ value, label, disabled }) =>
+          <option disabled={disabled?.()} value={value}>{label}</option>}
+      </For>
+    </select>
+  </div>
+}
