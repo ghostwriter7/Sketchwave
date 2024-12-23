999b62d5294cbcd622319c64dec4c38b47f285f9
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-23 11:06:55 +0100

    Fixed rendering of linear gradients (II)

--- src/utils/canvas-rendering-context.extensions.ts
+++ src/utils/canvas-rendering-context.extensions.ts
@@ -19,7 +19,7 @@ const createGradient = function (this: CanvasRenderingContext2D | OffscreenCanva
 
   switch (gradient.gradientType) {
     case 'linear':
-      canvasGradient = this.createLinearGradient(0, 0, width, 0);
+      canvasGradient = this.createLinearGradient(origin.x, origin.y, origin.x + width, origin.y);
       break;
     case 'radial': {
       const centerX = origin.x + width / 2;
