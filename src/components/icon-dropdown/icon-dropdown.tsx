92d90fb1131357db357578dd32c113d56c5e79a7
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-12-27 14:58:27 +0100

    Improved positioning for IconDropdown

--- src/components/icon-dropdown/icon-dropdown.tsx
+++ src/components/icon-dropdown/icon-dropdown.tsx
@@ -1,4 +1,4 @@
-import { For, type VoidProps } from 'solid-js';
+import { For, type VoidProps, createUniqueId } from 'solid-js';
 import type { Options, OptionValue } from '../../types/core.type.ts';
 import styles from './icon-dropdown.module.css';
 import { Icon } from '../icon/icon.tsx';
@@ -13,19 +13,19 @@ export type IconDropdownProps<T = OptionValue> = {
 
 export const IconDropdown = (props: VoidProps<IconDropdownProps>) => {
   const popoverId = `icon-dropdown_${Math.random()}`;
-  const anchorName = `--anchor${Math.random()}`;
 
-  return <div class={styles.wrapper} style={{ anchorName }} >
-    <button title={props.title} popovertarget={popoverId}>
+  const anchorName = `--${createUniqueId()}`;
+  return <div class={styles.wrapper} >
+    <button title={props.title} popovertarget={popoverId} style={{'anchor-name': anchorName }}>
       <Icon icon={props.icon}/>
     </button>
-    <div class={styles.popver} popover id={popoverId}>
-      <Card title={props.title} style={{ 'position-anchor': anchorName, left: `anchor(left)`, top: `anchor(bottom)` }}>
-        <For each={props.options}>
-        {({ value, label, icon }) =>
-          <span>{label}</span>}
-      </For>
-      </Card>
+    <div popover id={popoverId} style={{'position-anchor': anchorName}} class={styles.popover} >
+        <Card title={props.title}>
+          <For each={props.options}>
+          {({ value, label, icon }) =>
+            <span>{label}</span>}
+        </For>
+        </Card>
     </div>
   </div>
 }
