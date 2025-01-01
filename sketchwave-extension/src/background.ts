5a52e4ea7f91114bb40d96bd71eb50b1f6194384
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-01-01 15:32:03 +0100

    Added mechanism to inject dataUrl into SketchWave

--- sketchwave-extension/src/background.ts
+++ sketchwave-extension/src/background.ts
@@ -1,10 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+  chrome.runtime.onMessage.addListener( (message, _sender, sendResponse) => {
     if (message.command === 'captureScreenshot') {
-      chrome.tabs.captureVisibleTab((result) => {
-        sendResponse({ success: true, result });
+      chrome.tabs.captureVisibleTab(async (result) => {
+        if (chrome.runtime.lastError) {
+          sendResponse({ success: false, error: chrome.runtime.lastError.message })
+        } else {
+          // const tab = await chrome.tabs.create({ url: `https://sketchwave.vercel.app` });
+          const tab = await chrome.tabs.create({ url: `http://localhost:5173` });
+          await chrome.scripting.executeScript({
+            args: [result],
+            func: (dataUrl: string)=> localStorage.setItem('injectedDataUrl', dataUrl),
+            target: {
+              tabId: tab.id!
+            }
+          });
+          sendResponse({ success: true, result });
+        }
       });
     }
+    return true;
   })
 });
 
