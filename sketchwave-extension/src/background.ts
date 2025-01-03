41be9f8d4d451b4a9f88ca6421519f7f6caf1a26
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-01-03 17:28:06 +0100

    Rendered preview and passed to worker its coords

--- sketchwave-extension/src/background.ts
+++ sketchwave-extension/src/background.ts
@@ -1,17 +1,37 @@
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
+
+chrome.runtime.onMessage.addListener((message: {
+  command: string,
+  payload: { origin: [number, number], width: number, height: number }
+}, _sender, sendResponse) => {
   if (message.command === 'captureScreenshot') {
     chrome.tabs.captureVisibleTab(async (result) => {
       if (chrome.runtime.lastError) {
         sendResponse({ success: false, error: chrome.runtime.lastError.message })
       } else {
-        const tab = await chrome.tabs.create({ url: `https://sketchwave.vercel.app` });
-        await chrome.scripting.executeScript({
-          args: [result],
-          func: (dataUrl: string) => sessionStorage.setItem('dataUrl', dataUrl),
-          target: { tabId: tab.id! },
-          injectImmediately: true
-        });
-        sendResponse({ success: true });
+        const { height, width, origin: [x, y] } = message.payload;
+        const offscreenCanvas = new OffscreenCanvas(width, height);
+        const ctx = offscreenCanvas.getContext('2d')!;
+        const response = await fetch(result);
+        const blob = await response.blob();
+        const imageBitmap = await createImageBitmap(blob);
+
+        ctx.drawImage(imageBitmap, x, y, width, height, 0, 0, width, height);
+        const targetBlob = await offscreenCanvas.convertToBlob();
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = async () => {
+          const tab = await chrome.tabs.create({ url: `https://sketchwave.vercel.app` });
+          await chrome.scripting.executeScript({
+            args: [fileReader.result as string],
+            func: (dataUrl: string) => sessionStorage.setItem('dataUrl', dataUrl),
+            target: { tabId: tab.id! },
+            injectImmediately: true
+          });
+          sendResponse({ success: true });
+        }
+
+        fileReader.readAsDataURL(targetBlob);
       }
     });
   }
--- sketchwave-extension/src/contentScript.ts
+++ sketchwave-extension/src/contentScript.ts
@@ -1,7 +1,77 @@
 document.addEventListener('keydown', async (event) => {
   if (event.ctrlKey && event.shiftKey && event.code === 'KeyK') {
-    chrome.runtime.sendMessage({ command: 'captureScreenshot' }, (response) => {
-      console.log(response);
-    });
+    const canvas = document.createElement('canvas');
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    canvas.style.position = 'absolute';
+    (canvas.style.zIndex as unknown as number) = 10000000;
+    (canvas.style.inset as unknown as number) = 0;
+    document.body.style.overflow = 'hidden';
+
+    document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext('2d')!;
+
+    const renderTransparentBackground = () => {
+      ctx.fillStyle = 'rgba(0, 0, 0, .25)';
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    };
+
+    renderTransparentBackground();
+
+    canvas.style.cursor = 'crosshair';
+
+    const points: {
+      startPoint?: [number, number];
+      endPoint?: [number, number];
+    } = {};
+
+    const getOriginAndDimensions = () => {
+      const { startPoint, endPoint } = points;
+      const origin = [Math.min(startPoint![0], endPoint![0]), Math.min(startPoint![1], endPoint![1])] as [number, number];
+
+      const width = Math.max(startPoint![0], endPoint![0]) - origin[0];
+      const height = Math.max(startPoint![1], endPoint![1]) - origin[1];
+
+      return { origin, width, height };
+    }
+
+    const renderPreview = () => {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      renderTransparentBackground();
+      const { origin, width, height } = getOriginAndDimensions();
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 0.10)';
+      ctx.fillRect(...origin, width, height);
+      ctx.strokeStyle = '#000';
+      ctx.lineWidth = 3;
+      ctx.strokeRect(...origin, width, height);
+    }
+
+
+    const abortController = new AbortController();
+    const options = { signal: abortController.signal };
+
+    canvas.addEventListener('mousedown', (event: MouseEvent) => {
+      points.startPoint = [event.offsetX, event.offsetY];
+    }, options);
+
+    canvas.addEventListener('mousemove', (event: MouseEvent) => {
+      if (event.buttons === 1 && points.startPoint) {
+        points.endPoint = [event.offsetX, event.offsetY];
+        renderPreview();
+      }
+    }, options);
+
+    canvas.addEventListener('mouseup', () => {
+      if (points.startPoint && points.endPoint) {
+        abortController.abort();
+        canvas.remove();
+        chrome.runtime.sendMessage({ command: 'captureScreenshot', payload: getOriginAndDimensions() },
+          (response) => {
+            console.log(response);
+          });
+      }
+    }, options);
   }
 });
