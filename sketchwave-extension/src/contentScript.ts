93db548303368e88d937728c0b16edbcddc03490
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-01-04 08:40:34 +0100

    ContentScript refactored and improved canvas positioning and point retrieval

--- sketchwave-extension/src/contentScript.ts
+++ sketchwave-extension/src/contentScript.ts
@@ -1,53 +1,56 @@
+type Point = [number, number];
+type Dimensions = { width: number; height: number };
+type OriginAndDimensions = { origin: Point } & Dimensions;
+type CapturedPoints = { startPoint: Point, endPoint: Point };
+
+function createCanvas(): HTMLCanvasElement {
+  const canvas = document.createElement('canvas');
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  canvas.style.position = 'fixed';
+  (canvas.style.zIndex as unknown as number) = 10000000;
+  (canvas.style.inset as unknown as number) = 0;
+  canvas.style.cursor = 'crosshair';
+
+  document.body.appendChild(canvas);
+  return canvas;
+}
+
+function renderTransparentBackground(ctx: CanvasRenderingContext2D): void {
+  ctx.fillStyle = 'rgba(0, 0, 0, .25)';
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function renderPreview(ctx: CanvasRenderingContext2D, dimensions: { origin: [number, number], width: number, height: number}): void {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  renderTransparentBackground(ctx);
+  const { origin, width, height } = dimensions;
+
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.10)';
+  ctx.fillRect(...origin, width, height);
+  ctx.strokeStyle = '#000';
+  ctx.lineWidth = 3;
+  ctx.strokeRect(...origin, width, height);
+}
+
+function getOriginAndDimensions(points: CapturedPoints): OriginAndDimensions {
+  const { startPoint, endPoint } = points;
+  const origin = [Math.min(startPoint![0], endPoint![0]), Math.min(startPoint![1], endPoint![1])] as [number, number];
+
+  const width = Math.max(startPoint![0], endPoint![0]) - origin[0];
+  const height = Math.max(startPoint![1], endPoint![1]) - origin[1];
+
+  return { origin, width, height };
+}
+
 document.addEventListener('keydown', async (event) => {
   if (event.ctrlKey && event.shiftKey && event.code === 'KeyK') {
-    const canvas = document.createElement('canvas');
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    canvas.style.position = 'absolute';
-    (canvas.style.zIndex as unknown as number) = 10000000;
-    (canvas.style.inset as unknown as number) = 0;
-    document.body.style.overflow = 'hidden';
-
-    document.body.appendChild(canvas);
-
+    const canvas = createCanvas();
     const ctx = canvas.getContext('2d')!;
 
-    const renderTransparentBackground = () => {
-      ctx.fillStyle = 'rgba(0, 0, 0, .25)';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-    };
+    renderTransparentBackground(ctx);
 
-    renderTransparentBackground();
-
-    canvas.style.cursor = 'crosshair';
-
-    const points: {
-      startPoint?: [number, number];
-      endPoint?: [number, number];
-    } = {};
-
-    const getOriginAndDimensions = () => {
-      const { startPoint, endPoint } = points;
-      const origin = [Math.min(startPoint![0], endPoint![0]), Math.min(startPoint![1], endPoint![1])] as [number, number];
-
-      const width = Math.max(startPoint![0], endPoint![0]) - origin[0];
-      const height = Math.max(startPoint![1], endPoint![1]) - origin[1];
-
-      return { origin, width, height };
-    }
-
-    const renderPreview = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      renderTransparentBackground();
-      const { origin, width, height } = getOriginAndDimensions();
-
-      ctx.fillStyle = 'rgba(255, 255, 255, 0.10)';
-      ctx.fillRect(...origin, width, height);
-      ctx.strokeStyle = '#000';
-      ctx.lineWidth = 3;
-      ctx.strokeRect(...origin, width, height);
-    }
-
+    const points: Partial<CapturedPoints> = {};
 
     const abortController = new AbortController();
     const options = { signal: abortController.signal };
@@ -59,7 +62,7 @@ document.addEventListener('keydown', async (event) => {
     canvas.addEventListener('mousemove', (event: MouseEvent) => {
       if (event.buttons === 1 && points.startPoint) {
         points.endPoint = [event.offsetX, event.offsetY];
-        renderPreview();
+        renderPreview(ctx, getOriginAndDimensions(points as CapturedPoints));
       }
     }, options);
 
@@ -67,10 +70,18 @@ document.addEventListener('keydown', async (event) => {
       if (points.startPoint && points.endPoint) {
         abortController.abort();
         canvas.remove();
-        chrome.runtime.sendMessage({ command: 'captureScreenshot', payload: getOriginAndDimensions() },
-          (response) => {
-            console.log(response);
-          });
+        const { origin, width, height } = getOriginAndDimensions(points as CapturedPoints);
+        const { devicePixelRatio } = window;
+        chrome.runtime.sendMessage({
+            command: 'captureScreenshot',
+            payload: {
+              origin: [origin[0] * devicePixelRatio, origin[1] * devicePixelRatio],
+              width: width * devicePixelRatio,
+              height: height * devicePixelRatio
+            }
+          },
+          (response) => console.log(response)
+        );
       }
     }, options);
   }
